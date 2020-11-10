8166fdddd6f7fbd0ebcb29407b00f6f2730a1745
ByeonGooHoon <dksekfldks65@naver.com>
2020-11-10 21:13:41 +0900

    Spring Data JPA Auditing 기능 추가

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,12 +2,26 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataJpaApplication.class, args);
+	}
+
+	@Bean
+	public AuditorAware<String> auditorAware(){
+		//보통은 여기에 스프링 시큐리티에서 꺼내서 등록하면 됨.
+		//자동으로 채워줌.
+		return () -> Optional.of(UUID.randomUUID().toString());
 	}
 
 }
